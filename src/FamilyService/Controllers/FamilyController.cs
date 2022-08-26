987dd2536a741d561781590b258e019131974aa9
Artem Lopatin <artx7@outlook.com>
2022-08-26 15:47:45 +0300

    family controller

---
+++ src/FamilyService/Controllers/FamilyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.FamilyService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FamilyController : ControllerBase
+    {
+        [HttpGet("get")]
+        public void GetKidsAsync(
+            [FromQuery] Guid employeeId)
+        {
+            
+        }
+    }
+}
